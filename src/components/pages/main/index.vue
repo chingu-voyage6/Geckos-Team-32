1612cab99dddd0170b61e0f1ea32296558b174e8
Onyinye Uche <sylon231@gmail.com>
2018-07-01 20:50:55 +0100

    Add header and footer components to main page

--- src/components/pages/main/index.vue
+++ src/components/pages/main/index.vue
@@ -1,14 +1,37 @@
 <template>
     <div>
-        main
+        <MainHeader></MainHeader>
+        <img src="../images/header-img.jpg" class="headerImg">
+
+        <MainFooter class="footer"></MainFooter>
+       
+
     </div>
 </template>
 
 <script>
+    import MainHeader from "+/MainHeader"
+    import MainFooter from "+/MainFooter"
+    
     export default {
+        components:{
+            MainHeader,
+            MainFooter
+        },
     }
+
 </script>
 
 <style scoped>
+.headerImg{
+    width: 100%;
+    height: 400px;
+}
 
+.footer{
+    position: relative;
+    top: 800px;
+   
+
+}
 </style>

a45da86a692d198f20a65a23b7ddfb1cbccfe21e
Onyinye Uche <sylon231@gmail.com>
2018-07-01 22:20:25 +0100

    Replicate footer mockup

---
+++ src/components/common/MainFooter.vue
@@ -0,0 +1,37 @@
+<template>
+<div class="container-fluid">
+     <div class="row">
+        <div class="col-sm-12 footer">
+            <p class="footerText">Chingu Geckos Team-32</p>
+        </div>
+    </div>
+</div>
+   
+</template>
+
+
+<script>
+export default {
+    
+}
+</script>
+
+
+
+<style scoped>
+.footer{
+    height: 60px;
+    background-color:#ff0000b7;
+     width:100%
+}
+
+.footerText{
+    text-align: center;
+    color:white;
+    position: relative;
+    top: 20px;
+}
+</style>
+
+
+</style>

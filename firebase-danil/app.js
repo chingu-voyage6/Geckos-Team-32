5d25cf816506da75529d290f2348d400ace075dd
dsklemeshov <38494549+dsklemeshov@users.noreply.github.com>
2018-07-02 14:21:27 +0300

    danil 02.07.2018

--- firebase-danil/app.js
+++ firebase-danil/app.js
@@ -1,34 +1,67 @@
-const recepieList = document.querySelector(".content");
+const showRecipesBtn = document.querySelector("#showRecipesBtn");
 
-function renderRecepis(doc) {
-    var recUl = document.createElement("ol");
-    recUl.textContent = doc.Title;
-    recepieList.appendChild(recUl);
-    var ingUl = document.createElement("ul");
-    ingUl.textContent = "Ingredients:";
+const userNameTD = document.querySelector("#userNameTD");
+const userFamilyNameTD = document.querySelector("#userFNTD");
+const userEmailTD = document.querySelector("#userEmailTD");
+const userLocTD = document.querySelector("#userLocTD");
 
-    doc.ingredients.forEach(ing => {
-        let ingli = document.createElement("li");
-        let docRef = db.collection("Ingredients").doc(ing.id);
-        docRef.get().then(
-            function (ingDoc) {
-                ingli.textContent = ingDoc.data().Name;
+const userEmail = document.querySelector("#userEmail");
 
-            })
-        let priceDoc = db.collection('prices').where('ingredient', '==', ing);
-        priceDoc.get()
-            .then(function (ingPrice) {
-                ingPrice.forEach(function (price) {
-                    ingli.textContent += " (price:" + price.data().price + ')';
-                });
-            })
-        ingUl.appendChild(ingli);
-        recUl.appendChild(ingUl);
+const errMsg = document.querySelector("#errorMsg");
+
+var locationFindedItem = "";
+var IsUerExist = false;
+
+showRecipesBtn.addEventListener("click", () => {
+  let userItem = db
+    .collection("users")
+    .where("email", "==", userEmail.value.trim());
+  console.log("1 " + userItem);
+  userItem.get().then(function(snapshot) {
+    console.log("2 " + snapshot);
+    if ((snapshot.docs.length = 0)) {
+      errMsg.textContent =
+        "The user with e-mail:" + userEmail.value.trim() + " does not exist!";
+    } else {
+      let ustItem = snapshot.docs[0].data();
+      console.log("3 " + ustItem);
+      userNameTD.textContent = ustItem.name;
+      userFamilyNameTD.textContent = ustItem.familyname;
+      userEmailTD.textContent = ustItem.email;
+      checkLocation(ustItem.location.id);
+      try {
+        userLocTD.textContent =
+          locationFindedItem.country.trim() + ", " + locationFindedItem.city.trim();
+      } catch {
+        userLocTD.textContent = "1";
+      }
+      let a = 0;
+    }
+  });
+});
+
+function checkLocation(locID) {
+  let localionItem = db.collection("locations");
+  //localionItem = localionItem.where("id", "==", locID.trim());
+  localionItem
+    .doc(locID.trim())
+    .get()
+    .then(snapshot => {
+      if (snapshot.exists) {
+        doc = snapshot.data();
+
+        console.log("4 " + doc);
+        locationFindedItem = doc;
+      }
     });
 }
 
-db.collection('recipe').get().then((snapshot) => {
+db.collection("recipe")
+  .get()
+  .then(snapshot => {
     snapshot.docs.forEach(doc => {
-        renderRecepis(doc.data());
+      let docData = doc.data();
+      IsUerExist = false;
+      console.log("0 " + docData);
     });
-});
+  });

d917a797ced524383eb1d5633d84ad35e65cc745
dsklemeshov <38494549+dsklemeshov@users.noreply.github.com>
2018-07-11 13:49:35 +0300

    Danil-11.07.2018-2

--- img_upload/getImages.js
+++ img_upload/getImages.js
@@ -2,17 +2,36 @@ const cloudName = "dkltr3zmo";
 const unsignedUploadPreset = "kj42vsaw";
 
 // *********** Get files ******************** //
-function handleFiles() {
-    let recipieId = document.getElementById('idInput').value;
+function getFiles() {
+  let recipieId = document.getElementById("idInput").value;
 
-    try {
-        getImages(recipieId);
-    } catch {
-        document.getElementById("header").textContent = "Something goes wrong.";
-        document.getElementById("header").style.color = "red";
-    }
-};
+  try {
+    getImages(recipieId);
+  } catch {
+    document.getElementById("header").textContent = "Something goes wrong.";
+    document.getElementById("header").style.color = "red";
+  }
+}
 
 function getImages(recipieId) {
+  // Create a request variable and assign a new XMLHttpRequest object to it.
+  var request = new XMLHttpRequest();
 
+  // Open a new connection, using the GET request on the URL endpoint
+  request.open(
+    "GET",
+    "https://677479621951343:5PumNgZTJk62RHJOaDZrewIEZsg@api.cloudinary.com/v1_1/dkltr3zmo/resources/search?expression=resource_type:image",
+    true
+  );
+ //CLOUDINARY_URL=cloudinary://677479621951343:5PumNgZTJk62RHJOaDZrewIEZsg@dkltr3zmo
+  request.onload = function() {
+    var data = JSON.parse(this.response);
+
+    //data.forEach(movie => {
+      // Log each movie's title
+    //  console.log(movie.title);
+    //});
+  };
+  // Send request
+  request.send();
 }

0d265d1ec89e852e3372413a93496c6c9324a12e
dsklemeshov <38494549+dsklemeshov@users.noreply.github.com>
2018-07-13 13:29:26 +0300

    Danil-13.07.2018

--- img_upload/getImages.js
+++ img_upload/getImages.js
@@ -1,4 +1,3 @@
-
 // *********** Get files ******************** //
 function getFiles() {
   let recipieId = document.getElementById("idInput").value;
@@ -12,41 +11,64 @@ function getFiles() {
 }
 
 function getImages(recipieId) {
-  // Create a request variable and assign a new XMLHttpRequest object to it.
-  //var request = new XMLHttpRequest();
 
-  // Open a new connection, using the GET request on the URL endpoint
-  //request.open(
-  //  "GET",
-  //  "https://677479621951343:5PumNgZTJk62RHJOaDZrewIEZsg@api.cloudinary.com/v1_1/dkltr3zmo/resources/search?expression=resource_type:image",
-  //  true
-  //);
-  //CLOUDINARY_URL=cloudinary://677479621951343:5PumNgZTJk62RHJOaDZrewIEZsg@dkltr3zmo
-  //request.onload = function() {
-  //  var data = JSON.parse(this.response);
-
-  //data.forEach(movie => {
-  // Log each movie's title
-  //  console.log(movie.title);
-  //});
-  // };
-  // Send request
-  // request.send();
   //1531390276433
   for (i = 1; i <= 10; i++) {
     let imgExist = checkUrl(
-      "https://res.cloudinary.com/dkltr3zmo/"+recipieId+"/" + String(i) + ".jpg"
+      "https://res.cloudinary.com/dkltr3zmo/" +
+        recipieId +
+        "/" +
+        String(i) +
+        ".jpg"
     );
+
+    if (!imgExist) {
+      imgExist = checkUrl(
+        "https://res.cloudinary.com/dkltr3zmo/" +
+          recipieId +
+          "/" +
+          String(i) +
+          ".jepg"
+      );
+    }
+
+    if (!imgExist) {
+      imgExist = checkUrl(
+        "https://res.cloudinary.com/dkltr3zmo/" +
+          recipieId +
+          "/" +
+          String(i) +
+          ".png"
+      );
+    }
+
+    if (!imgExist) {
+      imgExist = checkUrl(
+        "https://res.cloudinary.com/dkltr3zmo/" +
+          recipieId +
+          "/" +
+          String(i) +
+          ".gif"
+      );
+    }
     if (imgExist) {
       var img = new Image(); // HTML5 Constructor
-      img.src = "https://res.cloudinary.com/dkltr3zmo/"+recipieId+"/" + String(i) + ".jpg";
-      img.alt = "recipeImg"+String(i);
-      img.id = "recipeImg"+String(i);
-      let col=document.querySelector(".column");
+      img.src =
+        "https://res.cloudinary.com/dkltr3zmo/" +
+        recipieId +
+        "/" +
+        String(i) +
+        ".jpg";
+      img.alt = "recipeImg" + String(i);
+      img.id = "recipeImg" + String(i);
+      let col = document.querySelector(".column");
       col.appendChild(img);
       console.log(
-        "https://res.cloudinary.com/dkltr3zmo/"+recipieId+"/" +
-          String(i) + ".jpg" +
+        "https://res.cloudinary.com/dkltr3zmo/" +
+          recipieId +
+          "/" +
+          String(i) +
+          ".jpg" +
           "-Exist!"
       );
     }
@@ -63,7 +85,7 @@ function checkUrl(url) {
 
   if (request) {
     request.open("GET", url, false);
-    request.send(); 
+    request.send();
     if (request.status == 200) {
       return true;
     }

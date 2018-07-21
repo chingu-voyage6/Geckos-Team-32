6ef2c7ee54e837953b624a79ff5d654fe210b244
Paolo Floreano <paolotossico@hotmail.com>
2018-07-21 23:33:44 +0200

    Update index.vue
    render of a single recipe with image

--- src/components/pages/recipe/index.vue
+++ src/components/pages/recipe/index.vue
@@ -1,9 +1,22 @@
 <template>
-    <div>
-    {{recipe}}
+  
+    <div class="recipe" >
+      <div class="title" >
+        <h1>{{recipe.Title}}</h1> 
+      </div>
+      <div class="image" ><img src="./images/sven-481475-unsplash.jpg" />
+      </div>
+      <div class="list">
+        <ul>
+          <li class="elements">Ingredients: <span></span> {{recipe.Ingridients[0].name}},</li>
+          <li class="elements">{{recipe.Ingridients[0].quanity}} dl.</li>
+        </ul>
+
+      <div class="elements">Directions: <span></span> {{recipe.Directions}}</div>
+      </div>
     </div>
 </template>
-
+/*{{recipe}}*/
 <script>
 import { mapGetters } from "vuex";
 
@@ -24,4 +37,46 @@ export default {
 </script>
 
 <style scoped>
+
+.recipe{
+  width: 80%;
+  margin: auto;
+  margin-bottom: 20px;
+}
+
+.title {
+  font-family:Oleo Script,cursive; 
+  color: #d80404;
+  text-align: center;
+  padding: 30px;
+  letter-spacing: 2px;
+}
+
+.list{
+  margin: 40px 0;
+}
+
+ul{
+  margin-left: -40px;
+}
+
+.elements, span{
+  font-style: italic;
+  display: inline;
+  list-style: none;
+  font-family:Oleo Script,cursive; 
+  color: #d80404;
+  padding-right: 10px;
+
+}
+.image{
+  margin: auto;
+  text-align: center;
+}
+
+img{
+  height: 20em;
+  width: 20em;
+}
+
 </style>

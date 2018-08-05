b6fab491f988267d3b6e0e23d0c2ac5889900652
o.shevchuck <olegsid93@gmail.com>
2018-08-05 14:42:16 +0300

    upd(recipe):refactor

--- src/components/pages/recipe/index.vue
+++ src/components/pages/recipe/index.vue
@@ -1,27 +1,28 @@
 <template>
-  
-    <div class="recipe" >
-      <div class="title" >
-        <h1>{{recipe.Title}}</h1> 
-      </div>
-      <div class="descritption">
-        <div class="image" ><img src="./images/sven-481475-unsplash.jpg" />
-      </div>
-        <div class="list">
-          <ul>
-            <li class="elements" v-for="item in recipe.Ingridients">
-            Ingredients: <span></span> {{item.name}},
-            </li>       
-            <li class="elements" v-for="item in recipe.Ingridients">
-              {{item.quanity}} dl.</li>
-          </ul>
-            <div class="elements">Directions: <span></span> {{recipe.Directions}}
-            </div>
-        </div>
+  <div class="recipe" v-if="recipe">
+    <div class="title" >
+      <h2>{{recipe.Title}}</h2> 
+    </div>
+    <div class="descritption">
+      <div class="image" >
+        <img :src="recipe.ImageUrl" />
+    </div>
+      <div class="list">
+        <ul>
+          <li class="elements" v-for="item in recipe.Ingredients">
+            <span>{{item.name}} </span> 
+            <span>{{item.quantity.number}} </span> 
+            <span>{{item.quantity.unit}} </span>
+          </li>
+        </ul>
+          <div class="elements">
+            <span>Directions:</span>
+            <span>{{recipe.Directions}}</span>
+          </div>
       </div>
     </div>
+  </div>
 </template>
-/*{{recipe}}*/
 <script>
 import { mapGetters } from "vuex";
 
@@ -42,51 +43,47 @@ export default {
 </script>
 
 <style scoped>
-
-.recipe{
+.recipe {
   width: 80%;
   margin: auto;
   margin-bottom: 20px;
 }
 
-.descritption{
+.descritption {
   display: flex;
 }
 
 .title {
-  font-family:Oleo Script,cursive; 
+  font-family: Oleo Script, cursive;
   color: #d80404;
   text-align: center;
   padding: 30px;
   letter-spacing: 2px;
 }
 
-.list{
+.list {
   width: 40%;
 }
 
-ul{
+ul {
   margin-left: -40px;
 }
 
-.elements, span{
+.elements {
   font-style: italic;
-  display: inline;
   list-style: none;
-  font-family:Oleo Script,cursive; 
+  font-family: Oleo Script, cursive;
   color: #d80404;
   padding-right: 10px;
-
 }
-.image{
+.image {
   width: 50%;
   margin: auto;
   text-align: center;
 }
 
-img{
+img {
   height: 20em;
   width: 20em;
 }
-
 </style>
